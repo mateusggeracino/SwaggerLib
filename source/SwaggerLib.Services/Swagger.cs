f27694659bb7699c71c5d0157ec52fc771fb75ae
Mateus <mateusgoncalvesgeracino@hotmail.com>
2019-08-23 14:33:15 -0300

    initial commit

---
+++ source/SwaggerLib.Services/Swagger.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SwaggerLib.Services
+{
+    public static class Swagger
+    {
+        public static void ConfigureServicesSwagger(this IServiceCollection services)
+        {
+
+        }
+
+        public static void ConfigureAppSwagger(this IApplicationBuilder app)
+        {
+
+        }
+    }
+}
